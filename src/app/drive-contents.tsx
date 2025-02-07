823963606b071615be31a21ddec68f64301b3e4a
akdevv <akdev5520@gmail.com>
2025-02-07 19:52:08 +0530

    added cleark auth

--- src/app/drive-contents.tsx
+++ src/app/drive-contents.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { Button } from "~/components/ui/button";
-import { FileRow, FolderRow } from "./file-row";
-import { Upload, ChevronRight } from "lucide-react";
-import { files_table, folders_table } from "~/server/db/schema";
 import Link from "next/link";
+import { ChevronRight } from "lucide-react";
+import { FileRow, FolderRow } from "./file-row";
+import { files_table, folders_table } from "~/server/db/schema";
+import { SignedIn, SignedOut, UserButton, SignInButton } from "@clerk/nextjs";
 
 export default function DriveContents(props: {
   files: (typeof files_table.$inferSelect)[];
@@ -35,13 +35,14 @@ export default function DriveContents(props: {
               </div>
             ))}
           </div>
-          <Button
-            onClick={handleUpload}
-            className="bg-blue-600 text-white hover:bg-blue-700"
-          >
-            <Upload className="mr-2" size={20} />
-            Upload
-          </Button>
+          <div>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
         </div>
         <div className="rounded-lg bg-gray-800 shadow-xl">
           <div className="border-b border-gray-700 px-6 py-4">
